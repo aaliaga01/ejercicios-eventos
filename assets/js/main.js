14a8729f67626d2359bfcec9f43c12c128e5c714
aaliaga <a.aliaga.val@gmail.com>
2017-05-16 13:54:22 -0400

    funciona boton 1

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function demoBoton(){
+
+	var demo = document.getElementById("demo");
+	demo.addEventListener('click', function(){ // digo a menu. agrego un evento ('tipo de evento', funcion).
+		alert("Holamundo");
+	});
+
+}
+demoBoton();
