b1ac275a1b68746d400f0f74bbbe98919c20231a
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-16 22:20:31 -0300

    funciona todo T_T

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
+//mensaje al cargar la página
+window.onload = function(){
+	alert('En línea');
+}
 
+//mensaje al pulsar el boton
 function demoBoton(){
-
 	var demo = document.getElementById("demo");
-	demo.addEventListener('click', function(){ // digo a menu. agrego un evento ('tipo de evento', funcion).
-		alert("Holamundo");
+	demo.addEventListener('click', function(){
+		alert("Hola mundo");
 	});
 
 }
 demoBoton();
+
+//mensaje al estar sobre caja de texto
+function ratonIn(){
+		alert("Estas sobre mí");
+}
+document.getElementById("raton").onmouseover = ratonIn;
+
+//mensaje al estar fuera de la caja de texto
+function ratonOut(){
+		alert("No estas sobre mí");
+}
+document.getElementById("raton").onmouseout = ratonOut;
+
